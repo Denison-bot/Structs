ffb1adc53c710a8ff5a42ad16ec1ee9b9437217c
Denison Percey <denisonipercey@outlook.com>
2022-01-17 17:06:23 -0400

    notes

--- Structs/Program.cs
+++ Structs/Program.cs
@@ -8,8 +8,42 @@ namespace Structs
 {
     class Program
     {
+        struct Vector3
+        {
+            public float x;
+            public float y;
+            public float z;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Structs:");
+            Console.WriteLine("");
+
+            Console.WriteLine("BeforeStructs:");
+            Console.WriteLine("");
+
+            // gamepad (2D)
+            float gamepadX;
+            float gamepadY;
+
+            gamepadX = 0.0f;
+            gamepadY = 0.0f;
+
+            // player movement (3D)
+            float playerMovementX;
+            float playerMovementY;
+            float playerMovementZ;
+
+            playerMovementX = gamepadX;
+            playerMovementY = 0.0f;
+            playerMovementZ = gamepadY;
+
+            Console.WriteLine("AfterStructs:");
+            Console.WriteLine("");
+
+
+            Console.ReadKey(true);
         }
     }
 }
